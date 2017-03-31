ac122f5c89d9bdbc93805e5d9eaa8a94ebc603a8
adiabat <rx@awsomnet.org>
2017-03-31 01:10:50 -0400

    fix typo
    (this typo was not detected with BCH coding...)

--- bech32.go
+++ bech32.go
@@ -98,7 +98,7 @@ func StringToSquashedBytes(input string) ([]byte, error) {
 }
 
 // PolyMod takes a byte slice and returns the 32-bit BCH checksum.
-// Note that the input byets to PolyMod need to be squashed to 5-bits tall
+// Note that the input bytes to PolyMod need to be squashed to 5-bits tall
 // before being used in this function.  And this function will not error,
 // but instead return an unsuable checksum, if you give it full-height bytes.
 func PolyMod(values []byte) uint32 {
